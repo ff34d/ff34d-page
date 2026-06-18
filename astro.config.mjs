7e5a0ee241964e58fc50430dd9384f55e6087b48
ff34d <urajimirudev@gmail.com>
2026-06-18 16:40:58 +0400

    chore(app): add support mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config"
-
 import tailwindcss from "@tailwindcss/vite"
-
 import react from "@astrojs/react"
+import mdx from "@astrojs/mdx"
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +10,6 @@ export default defineConfig({
       plugins: [tailwindcss()],
    },
 
-   integrations: [react()],
+   integrations: [react(), mdx()],
 })
+
