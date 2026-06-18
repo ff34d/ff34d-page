644db685a6248befe0c4047db152b9dbde888471
ff34d <urajimirudev@gmail.com>
2026-06-19 01:01:40 +0400

    style(theme-switch): add icons

--- src/components/features/ThemeSwitch.tsx
+++ src/components/features/ThemeSwitch.tsx
@@ -1,4 +1,5 @@
 import { useTheme } from "@/providers/ThemeProvider"
+import { MoonIcon, SunMediumIcon } from "lucide-react"
 import { useState } from "react"
 import { Switch } from "../ui/switch"
 
@@ -12,10 +13,24 @@ function ThemeSwitch() {
    }
 
    return (
-      <Switch
-         checked={isActive}
-         onCheckedChange={handleCheckedChange}
-      />
+      <div className="flex items-center justify-center gap-1">
+         <Switch
+            size="lg"
+            checked={isActive}
+            onCheckedChange={handleCheckedChange}>
+            {isActive ? (
+               <MoonIcon
+                  className="absolute top-1/2 left-1/2 -translate-1/2"
+                  size={18}
+               />
+            ) : (
+               <SunMediumIcon
+                  className="absolute top-1/2 left-1/2 -translate-1/2"
+                  size={18}
+               />
+            )}
+         </Switch>
+      </div>
    )
 }
 
--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -2,7 +2,7 @@ import ThemeSwitch from "../features/ThemeSwitch"
 
 function Header() {
    return (
-      <header className="w-full flex">
+      <header className="sticky top-0 left-0 flex w-full items-center justify-between gap-2 bg-black/25 px-6 py-4 backdrop-blur-xs">
          <h1>Vladimir Volkov</h1>
 
          <ThemeSwitch />
