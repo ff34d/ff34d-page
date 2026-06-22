24ac480b1e5e203b49f68e79cb0c0af8bdb7a8b3
ff34d <urajimirudev@gmail.com>
2026-06-22 23:31:58 +0400

    style(footer): add border top

--- src/components/widgets/Footer.tsx
+++ src/components/widgets/Footer.tsx
@@ -2,7 +2,7 @@ import { Box } from "../ui/box"
 
 export function Footer() {
    return (
-      <footer className="w-full text-center">
+      <footer className="w-full border-t text-center">
          <Box className="py-10">
             <span className="text-xs opacity-50">
                © 2026 Vladimir Volkov (ff34d). All rights reserved.
