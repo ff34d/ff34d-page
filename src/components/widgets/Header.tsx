e8dcd6ab64bc19c394c585ae2b34285064a5fd39
ff34d <urajimirudev@gmail.com>
2026-06-22 23:21:37 +0400

    style(header): apply new styles

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -3,9 +3,18 @@ import { Box } from "../ui/box"
 
 function Header() {
    return (
-      <header className="sticky top-0 left-0 w-full bg-black/25 backdrop-blur-xs">
+      <header
+         className="sticky top-0 left-0 z-10 w-full border-b backdrop-blur-xs"
+         style={{ backgroundColor: "var(--header)" }}>
          <Box className="flex items-center justify-between py-6">
-            <h1>Vladimir Volkov</h1>
+            <h1 className="text-xl">
+               <span
+                  className="mr-0.5"
+                  style={{ color: "var(--accent-green)" }}>
+                  ~
+               </span>
+               <span className="font-medium text-white">VV</span>
+            </h1>
             <ThemeSwitch />
          </Box>
       </header>
