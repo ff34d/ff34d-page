588c6cb87cb41c8ae151f8fb1f20c165e60fdbc8
ff34d <urajimirudev@gmail.com>
2026-06-23 23:40:17 +0400

    chore(app): add robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,7 @@
 // @ts-check
 import mdx from "@astrojs/mdx"
 import react from "@astrojs/react"
+import sitemap from "@astrojs/sitemap"
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from "astro/config"
 
@@ -10,7 +11,7 @@ export default defineConfig({
       plugins: [tailwindcss()],
    },
 
-   integrations: [react(), mdx()],
+   integrations: [react(), mdx(), sitemap()],
    site: "https://ff34d.github.io",
    base: "/ff34d-page",
 })
